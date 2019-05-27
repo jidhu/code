e8183cc197abd3861c1907f8064757db9f8f95b9
Jidhu <jidhu.jbj@gmail.com>
2019-05-27 16:14:01 +0000

    codenvy

--- C/arg.c
+++ C/arg.c
@@ -1,11 +1,14 @@
 #include<stdio.h>
+#include<string.h>
 //#include<cs50.h>
 
-int main(int argc,string argv[])
+int main(int argc,char *argv[])
 {
     printf("Total strings are %i\n",argc);
-    for(int i=0;i<argc;i++)
+    for(int i=0;i<argc;++i)
     {
-        printf("%s\n",argv[i]);
+        for (int j=0;j<strlen(argv[i]);j++)
+            {printf("%c",argv[i][j]);}
+        printf("\n");
     }
 }
