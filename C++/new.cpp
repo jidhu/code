fc610afb3bb60da51ab2c203e781d3aec730da44
jidhu <jidhu.jbj@gmail.com>
2019-05-28 15:48:23 +0000

    new file added

---
+++ C++/new.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<iostream>
+#include<string.h>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout<<argc<<endl;
+    for(int i=0; i<argc;i++)
+    {
+        cout<<argv[i]<<endl;
+    }
+    return 0;
+}
