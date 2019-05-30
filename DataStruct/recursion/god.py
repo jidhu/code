8b7cf6fd6859cf59c9abe42b278ca5478f15947d
Jidhu <jidhu.jbj@gmail.com>
2019-05-30 15:56:03 +0000

    fun

--- DataStruct/recursion/god.py
+++ DataStruct/recursion/god.py
@@ -1,5 +1,10 @@
+def rec(n,k):
+    if n==1:
+        return n
+    return (rec(n-1,k)+ k - 1 )% n+1
+    
 n = 5
-k = 3
-
-a = [lambda n: n for n in range(1,n+1)]
+k = 2
+a = rec(n,k)
+# a = [lambda n: n for n in range(1,n+2)]
 print(a)
