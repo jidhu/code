d295f8eb485ebad31bfd6698367041b21caf1e7c
[jidhu] <[jidhu.jbj@gmail.com]>
2019-05-31 19:22:42 +0530

    recursion

--- DataStruct/recursion/god.py
+++ DataStruct/recursion/god.py
@@ -2,9 +2,7 @@ def rec(n,k):
     if n==1:
         return n
     return (rec(n-1,k)+ k - 1 )% n+1
-    
+
 n = 5
 k = 2
-a = rec(n,k)
-# a = [lambda n: n for n in range(1,n+2)]
-print(a)
+print(rec(n,k))
