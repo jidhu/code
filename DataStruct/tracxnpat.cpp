d86860ee4e7b40ebef2300e632c0ea00b2b0a097
jidhu <jidhu.jbj@gmail.com>
2019-08-24 23:57:02 +0530

    tracxn

--- DataStruct/tracxnpat.cpp
+++ DataStruct/tracxnpat.cpp
@@ -9,29 +9,47 @@ int main()
     int s=n;
     int c=1;
 
-    for (row=1;row<2*n-1;row++)
+    for (row=1;row<=n;row++)
     {
-        if (s>=0)
+        for (space=s-1;space>0;space--)
         {
-            for (space=s-1;space>0;space--)
-            {
-                cout<<' ';
-            }
-
-
-            for (chr=1;chr<=c;c++)
-            {
-                cout<<'*';
-                if(chr<c)
-                {
-                    cout<<' ';
-                }
-            }
-            cout<<endl;
-
-            c++;
-            s--;
+            cout<<' ';
         }
+
+        for (chr=1;chr<=c;chr++)
+        {
+            cout<<'*';
+            if(chr<c)
+            {
+                cout<<'#';
+            }
+        }
+        cout<<endl;
+        c++;
+        s--;
     }
+        c--;
+        s+=2;
+
+    for (row=n;row<2*n-1;row++)
+    {
+        for (space=1;space<s;space++)
+        {
+            cout<<' ';
+        }
+
+        for (chr=c-1;chr>0;chr--)
+        {
+            cout<<'*';
+            if(chr>=c-1)
+            {
+                cout<<'#';
+            }
+        }
+        cout<<endl;
+        c--;
+        s++;
+    }
+
     return 0;
 }
