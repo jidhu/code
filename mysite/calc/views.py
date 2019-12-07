c463994a92c4afe84dc5d88742716cce50d1ae82
jidhu <jidhu.jbj@gmail.com>
2019-12-07 08:27:57 +0530

    atom

--- mysite/calc/views.py
+++ mysite/calc/views.py
@@ -3,4 +3,11 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home(request):
-    return HttpResponse("Hello World!")
+    return render(request, 'home.html', {'name':'Jidhu!!'})
+
+def add(request):
+    val1 = request.GET['num1']
+    val2 = request.GET['num2']
+    res = val1 + val2
+
+    return render(request, 'result.html',{'result':res})
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/dev/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parents[1]
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
