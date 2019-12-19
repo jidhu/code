b959538f302a8d27d8558d3dfa3470eb9540307e
jidhu <jidhu.jbj@gmail.com>
2019-12-19 12:07:08 +0530

    atom is ready

---
+++ DataStruct/Linked List/ll.py
@@ -0,0 +1,14 @@
+class Node:
+    def __init__(self):                     # constructor
+        self.data = None
+        self.next = None
+    def set_data(self, data):            #method for setting the data field of the Node
+        self.data = data
+    def get_data(self,data):            #method for getting the data field of the Node
+        return self.data
+    def set_next(self,next):            #method for setting the next field of the Node
+        self.next = next
+    def get_next(self,next):            #method for getting the next field of the Node
+        return self.next
+    def has_next(self):                   #returns true if the node points to another Node
+        return self.next != None``
---
+++ summa.py
@@ -0,0 +1,16 @@
+a = 5
+b = "vijay"
+c = True
+d = 45.5
+
+
+try:
+    if(a<2):
+        print(a)
+    a = a/0
+except:
+    ZeroDivisionError
+'''finally:
+    print("thapichita")
+    raise ("mavane maatunada")
+'''
