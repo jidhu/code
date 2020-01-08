7e7e8d761e32a3a9912dfb9c4f87f8329d5372cb
jidhu <jidhu.jbj@gmail.com>
2020-01-08 19:43:17 +0530

    chaos

--- hackerrank/interviewprep/arrays/newyrchaos.py
+++ hackerrank/interviewprep/arrays/newyrchaos.py
@@ -1,16 +1,28 @@
-n = 8
 arr = [1, 2, 5, 3, 7, 8, 6, 4]
+n = len(arr)
 chaos = 0
 
-i,j=0,0
-while i<n:
-    chaos = i+1 - arr[i]
-    if chaos > 2:
-        print("too")
-        break
-    if chaos <= 0:
-        i+=1
-        continue
-    j += chaos
-    i+=1
-print(j)
+def swap(a,b):
+    temp = a
+    a = b
+    b = temp
+
+i=n-1
+while(i>=0):
+    if (i+1) != arr[i]:
+        if (i-1) >= 0 and i+1 == arr[i-1]:
+                chaos += 1
+                arr[i],arr[i-1] = arr[i-1],arr[i]
+
+        elif (i-2) >= 0 and i+1 == arr[i-2]:
+                chaos += 2
+
+                arr[i-2],arr[i-1] = arr[i-1],arr[i-2]
+                arr[i-1],arr[i] = arr[i],arr[i-1]
+
+        else:
+            print('Too chaotic')
+            break
+
+    i-=1
+print(chaos)
