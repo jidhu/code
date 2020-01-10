80019289b1d67ec7d34879f86d56485d6cbc9c48
jidhu <jidhu.jbj@gmail.com>
2020-01-10 09:33:02 +0000

    adf

--- summa.py
+++ summa.py
@@ -1,16 +1,6 @@
-a = 5
-b = "vijay"
-c = True
-d = 45.5
-
-
-try:
-    if(a<2):
-        print(a)
-    a = a/0
-except:
-    ZeroDivisionError
-'''finally:
-    print("thapichita")
-    raise ("mavane maatunada")
-'''
+if __name__ == '__main__':
+    N = int(input())
+    list=[]
+    for i in N:
+        if i == "insert":
+            list.insert(i,e)
